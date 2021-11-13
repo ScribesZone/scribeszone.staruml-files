e6f36186d89aac5d822d70d4f14f120cef50a1ec
escribis <escribis@users.noreply.github.com>
2021-11-14 00:31:49 +0100

    initialize the repo

---
+++ README.rst
@@ -0,0 +1,28 @@
+staruml-files
+=============
+
+This extension allows to "attach" files to model element.
+The file can then be opened from StarUML.
+
+In practice the extension add two items in the contextual menu :
+
+*   ```Attach file```. This allows to add a file to the selected element.
+    A dialog is used to select an existing file.
+    In practice this command will tag the element with a tag ```file```.
+    The value of the tag is the filename.
+
+*   ```Open file```. This command opens the file attached to the
+    selected element if any. The "processor" used to open the file can
+    be configured in the configuration file : ```config/config.json```.
+    See this file for more information. You should adapt the content of
+    this file according to your platform, system settings, installed
+    software, etc.
+
+**NOTE 1**: Nothing is hidden : the tag "file" can be created manually and the
+value of these tags can be set in the property box of StarUML. The
+```Attach file``` command is just a convenient way to ensure that filenames
+correspond to actual files.
+
+**NOTE 2**: This is a very first version of this tool. Error handling is
+very poor. In case of problem it could be useful to have a look at the
+console using ```Debug > Show DevTools```.
---
+++ main.js
@@ -0,0 +1,212 @@
+
+/*global $, app, type*/
+
+const fs = require('fs')
+const path = require('path')
+const child_process = require("child_process");
+
+const configuration_filename = path.join(__dirname, 'config', 'config.json')
+
+// const use_command = '/home/favreje/SOFTWARE/use-5.1.0/bin/use  {{filename}}'
+// const code_command = 'code {{filename}}'
+// const xdg_command = 'xdg-open file:///{{filename}}'
+
+// app.platform
+
+var configuration
+
+function log(message) {
+    console.log('[files]: '+ message)
+}
+
+function readConfigurationFile() {
+    if (configuration === undefined
+            || configuration['reload'] === "true") {
+        try {
+            let text = fs.readFileSync(configuration_filename)
+            configuration = JSON.parse(text)
+            log('config: configuration file read')
+        } catch (error) {
+            app.error('Error while reading configuration file config/config.json')
+            log('config: cannot read configuration file')
+            log(error)
+        }
+    } else {
+        log('config: configuration file not read again')
+    }
+}
+
+function matchProcessorPattern(filename, pattern) {
+    // deal with extension field
+    const extension_json = pattern["extension"]
+    var extensions
+    if (extension_json === undefined) {
+        extensions = undefined
+    } else if (typeof extension_json === 'string') {
+        extensions = [extension_json]
+    } else {
+        extensions = extension_json
+    }
+    if (extensions === undefined) {
+        log(filename+'match: filename match no extension specified')
+        return true
+    } else {
+        const matching = extensions.includes(path.extname(filename))
+        log('match: '+filename+' '
+            +(matching?'match':"does not match ")
+            +' '+matching)
+        return matching
+    }
+}
+
+function getMatchingProcessorPattern(filename) {
+    readConfigurationFile()
+    for (const pattern of configuration["processors"]) {
+        if (matchProcessorPattern(filename, pattern)) {
+            return pattern
+        }
+    }
+    log('')
+    return undefined
+}
+
+function getCommandLine(filename) {
+    const pattern = getMatchingProcessorPattern(filename)
+    if (pattern === undefined) {
+        return undefined
+    } else {
+        return pattern['command'].replace('{{filename}}',filename)
+    }
+}
+
+function execute(command) {
+    child_process.exec(command,
+        (error, stdout, stderr) => {
+        if (error) {
+            log(`exec: error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            log(`exec: stderr: ${stderr}`);
+            return;
+        }
+        log(`exec: stdout: ${stdout}`);
+        return stdout
+    });
+}
+
+
+function getFilenameFromSelection() {
+    let error_msg
+    const selected_elements = app.selections.getSelectedModels()
+    if (selected_elements.length === 1) {
+        if (selected_elements[0].getTag('file')) {
+            const filename = selected_elements[0].getTagValue('file')
+            if (filename) {
+                log('filename: '+filename)
+                return filename
+            } else {
+                error_msg = "Tag file has no value. Should be a filename."
+            }
+        } else {
+            error_msg = "Tag 'file' not found in the selected element."
+        }
+    } else {
+        error_msg = "Please selected one element."
+    }
+    app.toast.error(error_msg, 20)
+    log('filename: ')
+    return null
+}
+
+
+
+
+function _handleOpen() {
+    const filename = getFilenameFromSelection()
+    if (filename) {
+        log('file = ' + filename)
+        const command = getCommandLine(filename)
+        if (command !== undefined) {
+            log( command)
+            app.toast.info("Opening " + command)
+            execute(command)
+            app.toast.info("Command executed")
+        } else {
+            const msg = "No processor available for " + filename
+            app.toast.error(msg)
+            log(msg)
+        }
+    } else {
+        const msg = 'No file to open'
+        app.toast.error(msg)
+        log(msg)
+    }
+}
+
+//-------------------------------------------------------------------------
+
+
+function createTag(element, name, value) {
+    var options = {
+        id: "Tag",
+        parent: element,
+        field: "tags",
+        modelInitializer: function (tag) {
+            tag.name = name
+            tag.kind = type.Tag.TK_STRING
+            tag.value = value
+        }
+    }
+    return app.factory.createModel(options)
+}
+
+/*
+Ensure that there is a tag file under the element with value=filename.
+If the tag file already exist change the value.
+Otherwise create a brand new tag file.
+Select the element at the end. This is necessary since the tag creation
+could have changed the selection.
+ */
+function ensureFileTagValue(element, filename) {
+    let tag = element.getTag('file')
+    if (tag === undefined) {
+        tag = createTag(element, 'file', filename)
+    }
+    app.engine.setProperty(tag, 'value',  filename)
+    app.modelExplorer.select(element, true)
+}
+
+function _handleAttach() {
+    const selected_element = app.selections.getSelectedModels()[0]
+    if (selected_element) {
+        // const selected_element = selected_elements[0]
+        // ask for a file
+        const files=app.dialogs.showOpenDialog('Select attachement')
+        if (files) {
+            filename = files[0]
+            ensureFileTagValue(selected_element, filename)
+            app.toast.info('File attached successfully')
+        } else {
+            // User cancel the attachement operation.
+            // Do not remove the file tag.
+        }
+    } else {
+        app.toast.warning('No element selected', 5)
+    }
+}
+
+
+//------------------------------------------------------------------------
+// Initilisation
+//------------------------------------------------------------------------
+
+function init() {
+    readConfigurationFile()
+    app.commands.register('files:attach', _handleAttach)
+    app.commands.register('files:open', _handleOpen)
+}
+
+exports.init = init
+
+/**/
